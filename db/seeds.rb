16fa58d133fb7700b357bc39b67e561843cbad73
amir2015 <zahranovich2@gmail.com>
2020-11-20 15:25:01 +0200

    routes done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#user1 = User.create(first_name:"ami",last_name:'ali')
+#course1 = Course.create(name:"cs")
+#e1=Enrollment.create(role:"teacher",course_id=course1.id,user_id=user1.id)
